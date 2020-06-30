c4d3e9127d85b9bf348af5f19f4ef0378e5a8aef
JoelGritter <joel@gritter.ca>
2020-06-29 20:57:47 -0400

    You -> Your Typo Fix

--- src/components/Competition/Compete/SolveAttempt.tsx
+++ src/components/Competition/Compete/SolveAttempt.tsx
@@ -136,7 +136,7 @@ export default function SolveAttempt({
 				<Grid container direction='row' justify='center' spacing={4}>
 					<Grid item>
 						<AttemptField
-							helperText={`Please enter you time for Solve ${
+							helperText={`Please enter your time for Solve ${
 								currentAttempt + 1
 							} with penalities applied. `}
 							eventId={round.event}
